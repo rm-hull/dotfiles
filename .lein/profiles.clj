d6a786edbd495252974439acc40b11a816ddb2c9
Richard Hull <rm_hull@yahoo.co.uk>
2013-07-21 01:14:59 +0100

    Add leiningen profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,16 @@
+{:user
+  {:plugins [
+    [lein-pprint "1.1.1"]
+    [lein-assoc "0.1.0"]
+    [jonase/eastwood "0.0.2"]
+    [lein-expectations "0.0.7"]
+    [lein-autoexpect "0.2.5"]
+    [lein-ancient "0.4.2"]]
+;  :dependencies [[clj-stacktrace "0.2.5"]]
+;  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+;                                       'print-cause-trace)
+;                      new (ns-resolve (doto 'clj-stacktrace.repl require)
+;                                      'pst)]
+;                 (alter-var-root orig (constantly @new)))]
+   }
+ }
