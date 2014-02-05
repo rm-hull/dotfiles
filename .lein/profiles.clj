6094610232689e8d60073715d858fad71f45be38
Richard Hull <rm_hull@yahoo.co.uk>
2014-02-05 00:32:43 +0000

    Added slamhound

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,8 +5,11 @@
     [jonase/eastwood "0.0.2"]
     [lein-expectations "0.0.7"]
     [lein-autoexpect "0.2.5"]
-    [lein-ancient "0.4.2"]]
-;  :dependencies [[clj-stacktrace "0.2.5"]]
+    [lein-ancient "0.5 4"]]
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+  :dependencies [[slamhound "RELEASE"]
+                 ;[clj-stacktrace "0.2.5"]
+                 ]
 ;  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
 ;                                       'print-cause-trace)
 ;                      new (ns-resolve (doto 'clj-stacktrace.repl require)
