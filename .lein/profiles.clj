63c29fdadfb664e889705d0116a05a474c0c55a4
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-01 23:20:25 +0100

    Refresh versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,24 +1,20 @@
-{:user
-  {:plugins [
-    [cider/cider-nrepl "0.11.0"]
-    [lein-cljfmt "0.3.0"]
-    [lein-pprint "1.1.1"]
+{
+ :user {
+  :plugins [
+    [cider/cider-nrepl "0.12.0"]
+    [lein-cljfmt "0.5.3"]
+    [lein-pprint "1.1.2"]
     [lein-assoc "0.1.0"]
     [lein-cloverage "1.0.6"]
-    [jonase/eastwood "0.0.2"]
-    [lein-expectations "0.0.7"]
-    [lein-autoexpect "0.2.5"]
-    [com.jakemccrary/lein-test-refresh "0.13.0"]
-    [lein-ancient "0.5.5"]]
-  :repl-options {:timeout 300000}
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-  :dependencies [[slamhound "RELEASE"]
-                 ;[clj-stacktrace "0.2.5"]
-                 ]
-;  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-;                                       'print-cause-trace)
-;                      new (ns-resolve (doto 'clj-stacktrace.repl require)
-;                                      'pst)]
-;                 (alter-var-root orig (constantly @new)))]
-   }
- }
+    [venantius/ultra "0.4.1"]
+    [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+    [lein-expectations "0.0.8"]
+    [lein-autoexpect "1.9.0"]
+    [com.jakemccrary/lein-test-refresh "0.14.0"]
+    [lein-ancient "0.6.10"]]
+  :repl-options {
+    :timeout 300000}
+  :aliases {
+    "slamhound" ["run" "-m" "slam.hound"]}
+  :dependencies [
+    [slamhound "1.5.5"]] } }
