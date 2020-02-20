5ce035059dd2e66513fdada278560b7e4e5c0ab3
Richard Hull <rm_hull@yahoo.co.uk>
2020-02-20 11:04:20 +0000

    Update leiningen dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {
  :user {
   :plugins [
-    [cider/cider-nrepl "0.18.0"]
-    [lein-cljfmt "0.6.1"]
-    [lein-pprint "1.2.0"]
+    [cider/cider-nrepl "0.23.0"]
+    [lein-cljfmt "0.6.6"]
+    [lein-pprint "1.3.2"]
     [lein-assoc "0.1.0"]
-    [lein-cloverage "1.0.13"]
+    [lein-cloverage "1.1.2"]
     [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
     [lein-expectations "0.0.8"]
     [lein-autoexpect "1.9.0"]
