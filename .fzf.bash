cd6ee51e632e6c8ed50c41bf4bb28b6b5707771a
Richard Hull <rm_hull@yahoo.co.uk>
2025-07-02 08:27:31 +0100

    Rework fzf shell integration

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,4 @@
-if [[ -d /usr/share/doc/fzf ]]; then
-  source "/usr/share/doc/fzf/examples/key-bindings.bash"
-  source "/usr/share/bash-completion/completions/fzf"
-elif [[ -d /opt/homebrew/opt/fzf/shell ]]; then
-  source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
-  source "/opt/homebrew/opt/fzf/shell/completion.bash"
-  if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-    PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-  fi
-fi
+eval "$(fzf --bash)"
 
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
