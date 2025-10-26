7a8795fdadc131e231e99b343809d57942ec1394
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-26 19:34:40 +0000

    Python-mode debarcle

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,13 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin main
 git submodule init
 git submodule update --init --recursive
-git submodule foreach git pull origin master
+git submodule foreach '
+  branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)
+  echo "Updating $name on branch $branch"
+  git fetch origin $branch
+  git checkout $branch
+  git pull origin $branch
+'
 
 cd .pyenv
 git pull origin master
